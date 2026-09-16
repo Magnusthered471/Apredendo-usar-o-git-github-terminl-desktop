5d58488ab8b0a944c5d6983f1aac49007001a3c0
magnusthered471 <ainzooalgown471@gmail.com>
2026-09-16 16:29:49 -0300

    Melhorando os comentarios
    Eu estou melhorando os comentarios pois estava meio vazio

--- new-coder/p1.c
+++ new-coder/p1.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
-#include <stidlib.h>
+#include <stdlib.h>
 // Hello amigo eu to comecando a usar de novo o code C/C++ ja que eu quero trabalhar em firmware.
+// Usando dois tipos de comentarios os // e os /* */
+/*
+int main(){
+    printf("Hello World\n");
+    return 0;
+
+}
+*/
