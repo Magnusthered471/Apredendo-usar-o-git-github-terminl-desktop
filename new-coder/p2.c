883339fc1ce87889347c5a6381e7deabfc05206f
magnusthered471 <ainzooalgown471@gmail.com>
2026-09-18 11:34:47 -0300

    Hello world, mis um dia apredendo usar o git/terminal bora la amigos, bora seguir em frente sem olhar para tras.

---
+++ new-coder/p2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+// aprendendo como mostrar uma mensagem na tela usando a linguagem C.
+int main(){
+    // o print do c e diferente do resto das linguagens, ele nao tem o mesmo nome, mas a ideia e a mesma.
+    printf("Hello world\n");
+    return 0;
+}
