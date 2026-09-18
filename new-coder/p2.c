440218ba527f55a244ae5e1a5b2c4d49b8e14d61
magnusthered471 <ainzooalgown471@gmail.com>
2026-09-18 11:50:11 -0300

    Arrumando poblemas de digitacao

--- new-coder/p2.c
+++ new-coder/p2.c
@@ -4,5 +4,6 @@
 int main(){
     // o print do c e diferente do resto das linguagens, ele nao tem o mesmo nome, mas a ideia e a mesma.
     printf("Hello world\n");
+    // ja vou partir para outra licao bora aprender c/c++ e e talves depois rust. 
     return 0;
 }
---
+++ new-coder/p3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+// eu to corrigindo aqui pois eu nao tinha me esquecido de colocar o main, e tambem deixado tudo meio patido.
+int main () {
+    return 0;
+}
